6f1f836c1de4f8ba1b1dba0a317f1c64397cb4c7
Jarek Jaśtak <jarek.jast@gmail.com>
2017-05-18 23:45:09 +0200

    Further validation and code cleaning. Adding class.

--- temp/ControlPreparationOfDataSet.py
+++ temp/ControlPreparationOfDataSet.py
@@ -1,27 +1,92 @@
 import sys
 import os
+class ControlPreparationOfDataSet():
 
-# Rozpoczecie testow
-print "\nChecking arguments"
-
-# Sprawdzenie liczby argumentow
-if len(sys.argv) != 4:
-    sys.exit('Number of Arguments: Fail!')
-print 'Number of Arguments: Pass'
-
-# Sprawdzenie istnienia sciezek dostepu
-pathToDirectoryWithEmotionTags = sys.argv[1]
-print pathToDirectoryWithEmotionTags
-if not(os.path.isdir(pathToDirectoryWithEmotionTags)):
-	sys.exit('Directory With Emotion Tags: Fail!')
-print 'Directory With Emotion Tags: Pass'
-pathToDirectoryWithPhotos = sys.argv[2]
-if not(os.path.isdir(pathToDirectoryWithPhotos)):
-	sys.exit('Directory With Photos: Fail!')
-print 'Directory With Photos: Pass'
-pathToDestinationDirectory = sys.argv[3]
-if not(os.path.isdir(pathToDirectoryWithEmotionTags)):
-	sys.exit('Directory With Emotion Tags: Fail!')
-print 'Directory With Emotion Tags: Pass'
+	# Sprawdzenie bibliotek 
+	def checkLibaries(self):
+		# Stale
+		dlibCheck ='Dlib installed:'
+		openCvCheck = 'OpenCv installed:'
+		globCheck = 'glob installed:'
+		shutilCheck = 'shutil installed:'		
+		print "\nChecking libraries"
+		
+		# OpenCv
+		try:
+			import cv2
+			result = True
+		except:
+			result = False;
+		self.checkResult(openCvCheck,result)
+		
+		# Dlib	
+		try:
+			import dlib
+			result = True
+		except:
+			result = False
+		self.checkResult(dlibCheck,result)
+		
+		# Glob	
+		try:
+			import glob
+			result = True
+		except:
+			result = False
+		self.checkResult(dlibCheck,result)
+		
+		# Copy	
+		try:
+			from shutil import copy
+			result = True
+		except:
+			result = False
+		self.checkResult(dlibCheck,result)
+		
 	
-print "\nChecking libraries"
+	# Sprawdzanie przeslanych argumentow
+	def checkArguments(self):
+		# Stale
+		argumentsNumberCheck = 'Number of Arguments:'
+		pathToDirectoryWithEmotionTagsCheck = 'Directory With Emotion Tags:'
+		pathToDirectoryWithPhotosCheck = 'Directory With Photos:'
+		pathToDestinationDirectoryCheck = 'Destination Path:'
+		print '\nChecking arguments'
+		
+		# Sprawdzenie liczby argumentow
+		self.checkResult(argumentsNumberCheck,True) if len(sys.argv) == 4 \
+		else self.checkResult(argumentsNumberCheck,False)
+
+		# Sprawdzenie istnienia sciezek dostepu
+		pathToDirectoryWithEmotionTags = sys.argv[1]
+		self.checkResult(pathToDirectoryWithEmotionTagsCheck,True) if(os.path.isdir(pathToDirectoryWithEmotionTags)) \
+		else self.checkResult(pathToDirectoryWithEmotionTagsCheck,False)
+		
+		pathToDirectoryWithPhotosCheck = sys.argv[2]
+		self.checkResult(pathToDirectoryWithPhotos,True) if(os.path.isdir(pathToDirectoryWithPhotos)) \
+		else self.checkResult(pathToDirectoryWithPhotosCheck,False)
+		
+		pathToDestinationDirectory = sys.argv[3]
+		self.checkResult(pathToDestinationDirectoryCheck,True) if(os.path.isdir(pathToDestinationDirectory)) \
+		else self.checkResult(pathToDestinationDirectoryCheck,False)
+		
+		
+	# Wyswietlanie wyniku testu	
+	def checkResult(self, testName, flag):
+		length = 35
+		resultFormatting = '%s %'+ str(length - len(testName)) + 's'
+		if flag == True:
+			result = 'Pass'	
+			print resultFormatting % (testName,result)	
+		else:
+			result = 'Fail'
+			sys.exit(resultFormatting % (testName,result))
+
+	def run(self):	
+		
+		# Testy
+		#self.checkLibaries()
+		self.checkArguments()
+		
+control = ControlPreparationOfDataSet()
+control.run()
--- temp/PrepareDataSet.bat
+++ temp/PrepareDataSet.bat
@@ -6,4 +6,11 @@ SET pathToDirectoryWithPhotos=C:\Ck-dataset\cohn-kanade-images
 :: Podaj bezwgledna sciezke do folderu do ktorego ma byc zapisany posortowany  zbior
 SET pathToDestinationDirectory=C:\organizedDataSet
 
+:: Sprawdzenie istnienia zainstalowanego pythona
+echo.
+echo Checking Python Module
+python --version 2>NUL
+if errorlevel 1 echo Python installed:                 Fail && exit /b
+echo Python installed:               Pass
+
 Python ControlPreparationOfDataSet.py %pathToDirectoryWithEmotionTags% %pathToDirectoryWithPhotos% %pathToDestinationDirectory%
